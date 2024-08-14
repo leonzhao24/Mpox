c7d516d5fc8456b4086ea92af5e3c5fc66491ff9
leonzhao24 <112895727+leonzhao24@users.noreply.github.com>
2024-08-14 16:54:39 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,4 +1,5 @@
 import streamlit as st
+import pandas as pd
 
 st.title("Mpox Dashboard")
 st.write(
@@ -6,7 +7,4 @@ st.write(
 )
 
 st.write("Here's our first attempt at using data to create a table:")
-st.write(pd.DataFrame({
-    'first column': [1, 2, 3, 4],
-    'second column': [10, 20, 30, 40]
-}))
+st.write(pd.DataFrame({'first column': [1, 2, 3, 4], 'second column': [10, 20, 30, 40]}))
