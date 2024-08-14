032f1cd2098977d3f3eff371e95384dfdf97258a
leonzhao24 <112895727+leonzhao24@users.noreply.github.com>
2024-08-14 16:53:39 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,3 +4,9 @@ st.title("Mpox Dashboard")
 st.write(
     "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
 )
+
+st.write("Here's our first attempt at using data to create a table:")
+st.write(pd.DataFrame({
+    'first column': [1, 2, 3, 4],
+    'second column': [10, 20, 30, 40]
+}))
